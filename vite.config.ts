c1969110f0e763275084eecb930a4a17f8bec11b
Vignesh <creativepointwebdev@gmail.com>
2026-03-30 17:41:01 +0530

    Redirection'

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,10 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  preview: {
+    port: 4173,
+    strictPort: true,
+  },
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean,
   ),
